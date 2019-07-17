7e9ec262db503e1d567e361b9f49c5c6984702e8
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-17 15:16:57 -0600

    Add tentative webscrape for img, title and currently working on date

--- scrape.js
+++ scrape.js
@@ -2,15 +2,16 @@ var Nightmare = require("nightmare");
 var nightmare = Nightmare({ show: true });
 
 nightmare
-  .goto("https://www.facebook.com/search/events/?q=Denver%20Drag&epa=SERP_TAB")
-  .wait("img")
-  
+  .goto("https://www.facebook.com/events/435603610346472/")
+  .wait("._63ew")
   .evaluate(() => {
-    var nameNodes = document.querySelectorAll("h1");
-    var list = [].slice.call(nameNodes);
-    return list.map(node => {
-      return node.innerText;
-    });
+    var img = document.querySelector(".scaledImageFitWidth");
+    var title = document.querySelector("#seo_h1_tag");
+    var date = document.querySelector('._xkh');
+    // var description = document.querySelector('');
+    return date
+    // return title.innerText
+    // return img.src;
   })
   .end()
   .then(result => {
